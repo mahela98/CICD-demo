063c5c24be75f87b5e7d75fbff1ca4945a44ba51
mahela98 <chandimaofficial@gmail.com>
2023-02-15 18:34:59 +0530

    u2

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with me chandima Mahela
+          Learn React
         </a>
       </header>
     </div>
